297c5bfe7f732c0d3bb8a2f4c5f0195f3b8a5e3b
Paula Ashraf <paula.a.pham@gmail.com>
2024-06-19 21:53:17 +0300

    add app bar to notes view

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -5,6 +5,10 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Nota'),
+      ),
+    );
   }
 }
