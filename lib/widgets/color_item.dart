23b110a65fcd18b319391495afbf5f3e7e128c76
Paula Ashraf <paula.a.pham@gmail.com>
2024-06-21 01:37:32 +0300

    create color item

---
+++ lib/widgets/color_item.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class ColorItem extends StatelessWidget {
+  final bool isActive;
+  final Color color;
+  const ColorItem({
+    super.key,
+    required this.isActive,
+    required this.color,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return isActive
+        ? CircleAvatar(
+            radius: 26.0,
+            backgroundColor: Colors.white,
+            child: CircleAvatar(
+              radius: 25.0,
+              backgroundColor: color,
+            ),
+          )
+        : CircleAvatar(
+            radius: 25.0,
+            backgroundColor: color,
+          );
+  }
+}
