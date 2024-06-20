28242930e1fb8bca7850327d90e472ca71b78cb7
Paula Ashraf <paula.a.pham@gmail.com>
2024-06-20 09:25:06 +0300

    create custom text form field

---
+++ lib/widgets/custom_text_form_field.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:nota/constants/colors.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  final TextEditingController textEditingController;
+  final int maxLines;
+  final double fontSize;
+  final String hintText;
+  final String? label;
+
+  const CustomTextFormField({
+    super.key,
+    required this.textEditingController,
+    required this.maxLines,
+    required this.fontSize,
+    required this.hintText,
+    this.label,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: textEditingController,
+      validator: (value) {
+        if (value?.isEmpty ?? true) {
+          return 'This field shouldn\'t be empty';
+        }
+        return null;
+      },
+      maxLines: maxLines,
+      cursorColor: kPrimaryColor,
+      keyboardType: TextInputType.text,
+      style: TextStyle(fontSize: fontSize),
+      textAlign: TextAlign.start,
+      decoration: InputDecoration(
+        hintText: hintText,
+        labelText: label,
+        hintStyle: TextStyle(fontSize: fontSize),
+        border: InputBorder.none,
+        errorBorder: InputBorder.none,
+        enabledBorder: InputBorder.none,
+        focusedBorder: InputBorder.none,
+        disabledBorder: InputBorder.none,
+        focusedErrorBorder: InputBorder.none,
+      ),
+    );
+  }
+}
