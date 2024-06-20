2f49e8adec1bf130264833d14cb213d97bc7db8c
Paula Ashraf <paula.a.pham@gmail.com>
2024-06-21 00:30:53 +0300

    add logic to edit note bottom sheet

--- lib/widgets/edit_note_bottom_sheet.dart
+++ lib/widgets/edit_note_bottom_sheet.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:nota/cubits/notes_cubit/notes_cubit.dart';
 import 'package:nota/models/note_model.dart';
 import 'package:nota/widgets/custom_filled_button.dart';
 import 'package:nota/widgets/custom_text_form_field.dart';
 
-class EditNoteBottomSheet extends StatelessWidget {
+class EditNoteBottomSheet extends StatefulWidget {
   final NoteModel note;
 
   const EditNoteBottomSheet({super.key, required this.note});
 
+  @override
+  State<EditNoteBottomSheet> createState() => _EditNoteBottomSheetState();
+}
+
+class _EditNoteBottomSheetState extends State<EditNoteBottomSheet> {
+  String? title, subTitle;
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -19,7 +26,9 @@ class EditNoteBottomSheet extends StatelessWidget {
       child: ListView(
         children: <Widget>[
           CustomTextFormField(
-            textEditingController: TextEditingController(text: note.title),
+            textEditingController:
+                TextEditingController(text: widget.note.title),
+            onChanged: (value) => title = value,
             maxLines: 1,
             fontSize: 20,
             hintText: 'Title',
@@ -28,7 +37,9 @@ class EditNoteBottomSheet extends StatelessWidget {
             height: 10.0,
           ),
           CustomTextFormField(
-            textEditingController: TextEditingController(text: note.subTitle),
+            textEditingController:
+                TextEditingController(text: widget.note.subTitle),
+            onChanged: (value) => subTitle = value,
             maxLines: 5,
             fontSize: 15,
             hintText: 'Sub Title',
@@ -39,7 +50,14 @@ class EditNoteBottomSheet extends StatelessWidget {
           Row(
             children: <Widget>[
               Expanded(
-                child: CustomFilledButton(onPressed: () {}, title: 'Save'),
+                child: CustomFilledButton(
+                    onPressed: () {
+                      widget.note.title = title ?? widget.note.title;
+                      widget.note.subTitle = subTitle ?? widget.note.subTitle;
+                      widget.note.save();
+                      NotesCubit.getNotesCubit(context).getAllNotes();
+                    },
+                    title: 'Save'),
               ),
             ],
           ),
