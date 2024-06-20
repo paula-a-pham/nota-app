075ba4b503895769f2a06ed7a5f93ca45107b858
Paula Ashraf <paula.a.pham@gmail.com>
2024-06-20 11:51:07 +0300

    create note model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,10 @@
+class NoteModel {
+  final String title;
+  final String subTitle;
+  final int color;
+  NoteModel({
+    required this.title,
+    required this.subTitle,
+    required this.color,
+  });
+}
